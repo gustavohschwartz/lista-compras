69d2b2b2919e38388b6a1c56c3f33d50975229ae
Valdiclei <cacausoares474@gmail.com>
2024-09-09 19:16:28 -0300

    Alterações do Projeto Gustavo Finalizadas

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", carregarItens);
 document.getElementById("formulario").addEventListener("submit", adicionarItem);
 document.getElementById("reset-lista").addEventListener("click", resetarLista);
 document.getElementById("limpar-lista").addEventListener("click", limparLista);
+document.getElementById("historico-lista").addEventListener("click", historico);
 
 function carregarItens() {
     const itens = JSON.parse(localStorage.getItem("itens")) || [];
@@ -24,7 +25,6 @@ function adicionarItemNaDOM(item) {
     const listaItens = document.getElementById("lista-itens");
     const li = document.createElement("li");
 
-    // Criar checkbox
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.checked = item.comprado;
@@ -33,31 +33,43 @@ function adicionarItemNaDOM(item) {
         atualizarEstiloItem(li, checkbox.checked);
     };
 
-    // Criar texto do item
     const textoItem = document.createElement("span");
     textoItem.textContent = item.nome;
     if (item.comprado) {
         textoItem.classList.add("riscado");
     }
 
-    // Criar botão remover
+    const itemContainer = document.createElement("div");
+    itemContainer.classList.add("item-container");
+    itemContainer.appendChild(checkbox);
+    itemContainer.appendChild(textoItem);
+
     const botaoRemover = document.createElement("button");
     botaoRemover.textContent = "Remover";
     botaoRemover.onclick = function () {
         removerItem(item.nome, li);
     };
 
-    li.appendChild(checkbox);
-    li.appendChild(textoItem);
+    li.appendChild(itemContainer);
     li.appendChild(botaoRemover);
     listaItens.appendChild(li);
 }
+
 
 function salvarItemNoLocalStorage(item) {
     const itens = JSON.parse(localStorage.getItem("itens")) || [];
     itens.push(item);
     localStorage.setItem("itens", JSON.stringify(itens));
+
+    const historicoItens = JSON.parse(localStorage.getItem("historico")) || [];
+    
+    const itemJaNoHistorico = historicoItens.some(historicoItem => historicoItem.nome === item.nome);
+    if (!itemJaNoHistorico) {
+        historicoItens.push(item);
+        localStorage.setItem("historico", JSON.stringify(historicoItens));
+    }
 }
+
 
 function atualizarStatusNoLocalStorage(nomeItem, comprado) {
     let itens = JSON.parse(localStorage.getItem("itens")) || [];
@@ -91,17 +103,20 @@ function resetarLista() {
     itens = itens.map((item) => ({ ...item, comprado: false }));
     localStorage.setItem("itens", JSON.stringify(itens));
 
-    // Atualizar o estilo dos itens
     const listaItens = document.getElementById("lista-itens");
     listaItens.querySelectorAll("span").forEach((span) => {
         span.classList.remove("riscado");
     });
 }
 
-// Função para limpar a lista
 function limparLista() {
     const listaItens = document.getElementById("lista-itens");
     listaItens.innerHTML = ""; 
 
     localStorage.removeItem("itens");
+}
+
+function historico(){
+    const historico = document.getElementById("historico-lista");
+    window.location.href = './html/historico.html'
 }
